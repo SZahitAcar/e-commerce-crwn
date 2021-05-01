0159af17dfb4861a80e13dccc744a1a7afc372f6
SZahitAcar <zahitacar@yandex.com>
2021-05-02 02:14:16 +0300

    adding header for SPA

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { HomepageComponent } from "./pages/homePage/HomepageComponent";
 import { Switch, Route } from "react-router-dom";
 import ShopPageComponent from "./pages/shopPage/ShopPageComponent";
+import { HeaderComponent } from "./components/header/HeaderComponent";
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <Switch>
         <Route exact path="/" component={HomepageComponent} />
         <Route path="/shop" component={ShopPageComponent} />
